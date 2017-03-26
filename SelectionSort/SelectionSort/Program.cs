5804d152503fb78ca11a0da1fb2b5dcf07e92ae6
Krunal Yadav <krunal.yadav18@gmail.com>
2017-03-26 17:53:56 +0530

    selection sort added

---
+++ SelectionSort/SelectionSort/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace SelectionSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int min, index, temp;
+            Random rad = new Random();
+            int[] ary = new int[10];
+            for (int i = 0; i < 10; i++)
+            {
+                ary[i] = rad.Next(1, 100);
+            }
+
+            Console.WriteLine("Unsorted integers");
+            Console.WriteLine(string.Join(",", ary));
+
+            for (int i = 0; i < ary.Length; i++)
+            {
+                min = ary[i];
+                index = i;
+                if (i == (ary.Length - 1))
+                    break;
+                for (int j = i + 1; j < ary.Length; j++)
+                {
+                    if (ary[j] < min)
+                    {
+                        min = ary[j];
+                        index = j;
+                    }
+                }
+                if (index != i)
+                {
+                    temp = ary[i];
+                    ary[i] = ary[index];
+                    ary[index] = temp;
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Sorted integers");
+            Console.WriteLine(string.Join(",", ary));
+
+            Console.Read();
+        }
+    }
+}
